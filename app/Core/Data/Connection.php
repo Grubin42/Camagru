9e8c45a20d08b96318ce02f34e02cc94937cc8cf
Jeremie Schreyer <jschreye@c1r4s2.42lausanne.ch>
2024-07-19 12:48:41 +0200

    archi

---
+++ app/Core/Data/Connection.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Camagru\Core\Data;
+
+class Connection
+{
+    function getDBConnection(): PDO {
+        try {
+            $dsn = 'pgsql:host=' . DB_HOST . ';dbname=' . DB_NAME;
+            $pdo = new PDO($dsn, DB_USER, DB_PASS);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        } catch (PDOException $e) {
+            die('Connection failed: ' . $e->getMessage());
+        }
+    }
+}
--- app/Presentation/index.php
+++ app/Presentation/index.php
@@ -1,18 +1,12 @@
 <?php
+
+namespace Camagru;
+
 require_once __DIR__ . '/config.php';
-function getDBConnection(): PDO {
-    try {
-        $dsn = 'pgsql:host=' . DB_HOST . ';dbname=' . DB_NAME;
-        $pdo = new PDO($dsn, DB_USER, DB_PASS);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $pdo;
-    } catch (PDOException $e) {
-        die('Connection failed: ' . $e->getMessage());
-    }
-}
+use Camagru\Core\Data\Connection;
 
 try {
-    $db = getDBConnection();
+    $db = Connection::getDBConnection();
     $stmt = $db->query('SELECT username, email FROM users');
     $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 } catch (Exception $e) {
