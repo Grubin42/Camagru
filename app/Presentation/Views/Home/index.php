f3fd7ce51f9c3cc4acd42dfc009c78ed3692c2b9
Jeremie Schreyer <jschreye@c1r4s2.42lausanne.ch>
2024-08-16 11:11:14 +0200

    fix:gitignore

--- app/Presentation/Views/Home/index.php
+++ app/Presentation/Views/Home/index.php
@@ -8,6 +8,6 @@
     Users
 </button>
 <div>
-    <?php include("../Users/Components/ListUsers.php")?>
+    <?php include(__DIR__ . "/../Users/Components/ListUsers.php") ?>
 </div>
 <h1>Liste des utilisateurs</h1>
