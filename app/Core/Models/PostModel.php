f73d1330765d605e89238494c6641453174599f9
Jérémie Schreyer <jeremieschreyer@gmail.com>
2024-08-25 16:15:10 +0200

    enregistrement image reussi

--- app/Core/Models/PostModel.php
+++ app/Core/Models/PostModel.php
@@ -3,6 +3,7 @@
 namespace Camagru\Core\Models;
 
 use PDO;
+use PDOException;
 use Camagru\Core\Data\Connection;
 
 class PostModel
@@ -14,11 +15,15 @@ class PostModel
         $this->db = Connection::getDBConnection();
     }
 
-    public function ImageRegister($imageContent)
-    {
-        // Insérez les données dans la base de données
-        $stmt = $this->db->prepare('INSERT INTO post (imageContent, user_id) VALUES (?, ?)');
-        $stmt->execute([null, 1]);
-        // Autres actions après l'insertion réussie
+    public function ImageRegister($imageContent) {
+        try {
+            // Insérez les données dans la base de données. Utilisez le `PDO::PARAM_LOB` pour indiquer qu'il s'agit d'un objet BLOB
+            $stmt = $this->db->prepare('INSERT INTO post (imageContent, user_id) VALUES (:imageContent, :user_id)');
+            $stmt->bindValue(':imageContent', $imageContent, PDO::PARAM_LOB);
+            $stmt->bindValue(':user_id', 1, PDO::PARAM_INT); // Assurez-vous que le user_id est valide
+            $stmt->execute();
+        } catch (PDOException $e) {
+            echo 'Échec de l\'insertion : ' . $e->getMessage();
+        }
     }
 }
